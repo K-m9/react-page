afb878840c6ef410ab8f1cc6e6509d7c5792abb4
K-m9 <62182658+K-m9@users.noreply.github.com>
2024-12-28 19:51:04 +0800

    update google analytics scripts for GTM and consent

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Home from './components/Home';
 import Work from './components/Work';
+import CookieConsent from "react-cookie-consent";
 
 const App = () => {
   return (
@@ -12,7 +13,25 @@ const App = () => {
       <Home />
       <Nav />
       <Work />
+      
+      <CookieConsent
+        debug={true}
+        buttonText="Accept"
+        cookieName="myAwesomeCookieName2"
+        style={{
+          textShadow: "2px 2px black",
+        }}
+        buttonStyle={{
+          color: "white",
+          fontWeight: "bolder",
+          textShadow: "2px 2px black",
+        }}
+      >
+        This website uses cookies to enhance the user experience.
+
+      </CookieConsent>
     </div>
+
   );
 };
 

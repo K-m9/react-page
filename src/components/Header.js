e2a6b5bbb8656ca6bdc95cbb650a3a3ef7f33565
K-m9 <62182658+K-m9@users.noreply.github.com>
2024-12-30 10:14:12 +0800

    update version

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='py-1'>
       <div className='container mx-auto'>
-        <p className='text-right text-gray-500 from-neutral-400 text-[14px]'>Version 0.1</p>
+        <p className='text-right text-gray-500 from-neutral-400 text-[14px]'>Version 0.2</p>
       </div>
     </header>
   );
